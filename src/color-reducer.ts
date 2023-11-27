162ef6c2a42184955cb9621db2c465cfc1c282bf
Clayton Stewart <claytons1212@gmail.com>
2023-11-27 15:11:37 -0700

    rgb fun

---
+++ src/color-reducer.ts
@@ -0,0 +1,39 @@
+import { rgb } from 'color-convert';
+
+type UpdateHexColorAction = {
+  type: 'update-hex-color';
+  payload: {
+    hexColor: string;
+  };
+};
+
+type UpdateRGBColorAction = {
+  type: 'update-rgb-color';
+  payload: { rgb: [number, number, number] };
+};
+
+type ColorState = {
+  hexColor: string;
+};
+
+export type AdjustColorActions = UpdateHexColorAction | UpdateRGBColorAction;
+
+export const initialState: ColorState = {
+  hexColor: '#11BB33',
+};
+
+export const colorReducer = (
+  state: ColorState = initialState,
+  action: AdjustColorActions,
+) => {
+  if (action.type === 'update-hex-color') {
+    const { hexColor } = action.payload;
+    return { ...state, hexColor };
+  }
+  if (action.type === 'update-rgb-color') {
+    const hexColor = '#' + rgb.hex(action.payload.rgb);
+    return { ...state, hexColor };
+  }
+
+  return state;
+};
