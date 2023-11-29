f3118727a48733fc068c02eb415be5c18375a4b4
Clayton Stewart <claytons1212@gmail.com>
2023-11-29 14:55:34 -0700

    types

--- src/context.tsx
+++ src/context.tsx
@@ -1,26 +1,23 @@
-import { Dispatch, PropsWithChildren, createContext, useReducer } from 'react';
+import { Dispatch, PropsWithChildren, useReducer } from 'react';
 import {
   AdjustColorActions,
   colorReducer,
   initialState,
 } from './color-reducer';
+import { createContext } from './create-context';
 
 type ColorContextState = {
   hexColor: string;
   dispatch: Dispatch<AdjustColorActions>;
 };
 
-export const ColorContext = createContext<ColorContextState>({
-  hexColor: '#FFADEF',
-} as ColorContextState);
+export const [useContext, ContextProvider] = createContext<ColorContextState>();
 
 export const ColorProvider = ({ children }: PropsWithChildren) => {
   const [state, dispatch] = useReducer(colorReducer, initialState);
   const hexColor = state.hexColor;
 
   return (
-    <ColorContext.Provider value={{ hexColor, dispatch }}>
-      {children}
-    </ColorContext.Provider>
+    <ContextProvider value={{ hexColor, dispatch }}>{children}</ContextProvider>
   );
 };
